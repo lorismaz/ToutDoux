9ce2305c147c41e07e47bf35517b184d554f8e5b
Loris Mazloum <loris.mazloum@gmail.com>
2016-08-28 18:36:05 -0700

    Removing not needed comments

--- ToDoList/ToDoList/ListTableViewController.swift
+++ ToDoList/ToDoList/ListTableViewController.swift
@@ -46,12 +46,12 @@ class ListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return lists.count
     }
 
