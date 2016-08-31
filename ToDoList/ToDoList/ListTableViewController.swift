b8e4aa7877e2f55890b7b4d0382cce610d25377d
Loris Mazloum <loris.mazloum@gmail.com>
2016-08-30 20:21:17 -0700

    Allow cell order rearrangement

--- ToDoList/ToDoList/ListTableViewController.swift
+++ ToDoList/ToDoList/ListTableViewController.swift
@@ -87,20 +87,20 @@ class ListTableViewController: UITableViewController {
         }    
     }
 
-    /*
+
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
 
-    /*
+
+
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
